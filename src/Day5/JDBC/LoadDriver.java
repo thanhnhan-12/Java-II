ee9075b0fae095e8fd4a7f41a74f33e49103efc5
unknown <nguyenthanhnhan12701@gmail.com>
2023-04-10 20:45:12 +0700

    Delete Connect Database

--- src/Day5/JDBC/LoadDriver.java
+++ src/Day5/JDBC/LoadDriver.java
@@ -4,21 +4,22 @@ import java.sql.*;
 
 public class LoadDriver {
     public static void main(String[] args) {
-        String url = "jdbc:sqlserver://NGUYENTHANHNHAN//SQLEXPRESS;databaseName=StudentManagement";
+        String url = "jdbc:sqlserver://NGUYENTHANHNHAN//SQLEPRESS;databaseName=StudentManagement";
         String username = "sa";
         String password = "nhan";
-        
+
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        
+
             Connection connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connect Successfully!");
-        
+
             connection.close();
         } catch (SQLException e) {
             System.out.println("Error Connect Database: " + e.getMessage());
         } catch (ClassNotFoundException e) {
             System.out.println("JDBC Driver not found: " + e.getMessage());
         }
+
     }
 }
