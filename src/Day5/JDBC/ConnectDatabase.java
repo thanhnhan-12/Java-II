2c097432350d03529c9514c451b5933149d34142
unknown <thanhnhan-12>
2023-04-10 17:23:12 +0700

    Update

--- src/Day5/JDBC/ConnectDatabase.java
+++ src/Day5/JDBC/ConnectDatabase.java
@@ -1,11 +1,70 @@
 package Day5.JDBC;
 
-import java.sql.*;
+import java.util.Scanner;
 
 public class ConnectDatabase {
 
     public static void main(String[] args) {
-       
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap vao so bat ky: ");
+        int number = sc.nextInt();
+    
+        if(number >= 0) {
+          System.out.println("Day la so nguyen duong");
+        } else {
+          System.out.println("Day la so nguyen am");
+        }
+
+        if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+
+          if(number >= 0) {
+            System.out.println("Day la so nguyen duong");
+          } else {
+            System.out.println("Day la so nguyen am");
+          }
+    
+        sc.close();
 
     }
 
